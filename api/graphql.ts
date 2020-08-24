c2d69bfe87aeeea4027c3213ba08c82a705cfe4e
Guillaume L <guillaume@skillup.co>
2020-08-24 15:48:01 +0200

    [TRY] apollo micro

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,58 +1,44 @@
-import { NowRequest, NowResponse } from '@vercel/node';
 import { Document, model } from 'mongoose';
-import { graphql, buildSchema } from 'graphql';
 import { isUndefined } from 'lodash';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import { ApolloServer } from 'apollo-server-micro';
 
+import typeDefs from '../schemas/hikings.typeDefs';
 import { Point } from '../types';
 
 import '../config/mongo';
 
 const Hiking = model('Hiking');
 
-// Construct a schema, using GraphQL schema language
-const schema = buildSchema(
-  readFileSync(
-    resolve(__dirname, '../schemas/hiking.gql'),
-    'utf-8',
-  ),
-);
-
-interface HikingsProps {
+interface HikingsArgs {
   readonly limit?: number;
   readonly near?: Point['coordinates']; // [lng, lat]
 }
 
-// The root provides a resolver function for each API endpoint
-const root = {
-  hiking: async ({ id, url }): Promise<Document> => {
-    return id
-      ? Hiking.findById(id).exec()
-      : Hiking.findOne({ url }).exec();
-  },
-  hikings: async ({ limit = 10, near }: HikingsProps): Promise<Document[]> => {
-    if (isUndefined(near)) {
-      return Hiking.find({}).limit(limit);
-    }
+const resolvers = {
+  Query: {
+    hiking: async (_, { id, url }): Promise<Document> => {
+      return id
+        ? Hiking.findById(id).exec()
+        : Hiking.findOne({ url }).exec();
+    },
+    hikings: async (_, { limit = 10, near }: HikingsArgs): Promise<Document[]> => {
+      if (isUndefined(near)) {
+        return Hiking.find({}).limit(limit);
+      }
 
-    return Hiking.aggregate().near({
-      near,
-      distanceField: 'meta.distance', // required
-      maxDistance: 5000, // 5km
-      includeLocs: 'details.departure',
-      uniqueDocs: true,
-      spherical: true,
-      limit,
-    });
+      return Hiking.aggregate().near({
+        near,
+        distanceField: 'meta.distance', // required
+        maxDistance: 5000, // 5km
+        includeLocs: 'details.departure',
+        uniqueDocs: true,
+        spherical: true,
+        limit,
+      });
+    },
   },
 };
 
-export default (req: NowRequest, res: NowResponse): void => {
-  const { query } = req.body;
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-  // Run the GraphQL query and print out the response
-  graphql(schema, query, root).then((response) => {
-    return res.json(response);
-  });
-};
+export default apolloServer.createHandler({ path: '/api/graphql' });
